69941931c6d17e4f77eee4985f85c3fcb7dc19e0
tksat <satoko.takada@findy.co.jp>
2020-12-27 09:50:49 +0900

    seo対策の設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,8 @@
 module.exports = {
   siteMetadata: {
     title: `panda`,
+    description: `A simple description about pandas eating lots...`,
+    author: `gatsbyjs`,
   },
   plugins: [
     {
@@ -14,8 +16,16 @@ module.exports = {
       options: {
         name: `src`,
         path: `${__dirname}/src/`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#6b37bf`,
+        theme_color: `#6b37bf`,
+        display: `standalone`,
+        icon: `src/images/icon.png`
       },
     },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
     `gatsby-transformer-remark`,
     `gatsby-plugin-emotion`,
     {
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
@@ -8,6 +9,7 @@ export default function BlogPost({ data }) {
 
   return (
     <Layout>
+      <SEO title={post.frontmatter.title} description={post.excerpt} />
       <div>
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
@@ -26,6 +28,7 @@ export const query = graphql`
       fields{
         slug
       }
+      excerpt
     }
   }
 `
